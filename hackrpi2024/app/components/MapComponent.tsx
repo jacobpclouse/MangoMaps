0800cc9dbfa474345bab8b2f2282fa3f468484d9
Justin Chen <jchen4086@gmail.com>
2024-11-09 14:21:03 -0500

    merge nico's

--- hackrpi2024/app/components/MapComponent.tsx
+++ hackrpi2024/app/components/MapComponent.tsx
@@ -1,4 +1,4 @@
-// src/MapComponent.tsx
+
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'tailwindcss/tailwind.css';
@@ -9,12 +9,17 @@ const MapComponent: React.FC = () => {
     const mapContainerRef = useRef<HTMLDivElement>(null);
     const infoRef = useRef<HTMLDivElement>(null);
 
+    const bounds: [number, number, number, number] = [-74.0210, 40.6981, -73.8655, 40.9153];
+      
     useEffect(() => {
         const map = new mapboxgl.Map({
             container: mapContainerRef.current!,
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-98.5795, 39.8283], // Centered on the USA
-            zoom: 4
+            center: [-73.9654, 40.7829], // Centered on NYC
+            zoom: 15,
+            pitch: 60,
+            bearing: -20,
+            maxBounds: bounds
         });
 
         map.on('load', () => {
@@ -46,13 +51,20 @@ const MapComponent: React.FC = () => {
                 }
             });
 
-            // Add click event to display vulnerability scores
-            map.on('click', 'regions-layer', (e) => {
-                if (e.features && e.features[0] && e.features[0].properties) {
-                    const properties = e.features[0].properties;
-                    const vulnerabilityScore = properties.vulnerability_score; // Replace with your actual property name
+            // Insert a 3D building layer
+            map.addLayer({
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15, // Minimum zoom level for showing 3D buildings
+                'paint': {
+                'fill-extrusion-color': '#aaa',
+                'fill-extrusion-height': ['get', 'height'],
+                'fill-extrusion-base': ['get', 'min_height'],
+                'fill-extrusion-opacity': 0.6,
                 }
-                infoRef.current!.innerHTML = `Vulnerability Score: ${5}`;
             });
 
             // Change the cursor to a pointer when the mouse is over the regions layer
@@ -64,12 +76,12 @@ const MapComponent: React.FC = () => {
             map.on('mouseleave', 'regions-layer', () => {
                 map.getCanvas().style.cursor = '';
             });
-        });
 
-        map.on('click', (event) => {
-            const { lng, lat } = event.lngLat;
-            console.log("Longitude: " + lng + " Latitude: " + lat);
-        })
+            map.on('click', (event) => {
+                const { lng, lat } = event.lngLat;
+                console.log("Longitude: " + lng + " Latitude: " + lat);
+            })
+        });
 
         return () => map.remove();
     }, []);
